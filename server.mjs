d5359ecd01abd87ae161ec809f740c9f3ef1e02c
Marnn9 <maren.narvik@gmail.com>
2024-01-31 12:31:34 +0100

    before class

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import express from 'express' // Express is installed using npm
 import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users.
-import SuperLogger from './modules/SuperUser.mjs';
+import SuperLogger from './modules/SuperLogger.mjs';
 
 // Creating an instance of the server
 const server = express();
