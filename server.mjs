393215b2cfdc2fdf103d4c3b124fa620dec89c8b
Marnn9 <maren.narvik@gmail.com>
2024-03-16 21:17:35 +0100

    deleted some comments

--- server.mjs
+++ server.mjs
@@ -7,33 +7,25 @@ import printDeveloperStartupImportantInformationMSG from "./modules/developHelpe
 
 printDeveloperStartupImportantInformationMSG();
 
-// Creating an instance of the server
 const server = express();
-// Selecting a port for the server to use.
 const port = (process.env.PORT || 8080);
 server.set('port', port);
 
-//enable logging in to server
 const log = new SuperLogger();
 server.use(log.createAutoHTTPRequestLogger());
 
-// Defining a folder that will contain static files.
 server.use(express.static('public'));
 
 server.use(express.json());
 
-// Telling the server to use the USER_API (all urls that uses this code will have to have the /user after the base address)
 server.use("/user", USER_API);
 
-//server.use( validataUser, USER_API); //check reference
 
-// A get request handler example)
 server.get("/", (req, res, next) => {
     res.status(200).send(JSON.stringify({ msg: "Server ok?" })).end();
 });
 
 server.use(errorMiddleware);
-// Start the server 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
